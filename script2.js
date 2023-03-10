c1bd554e8016b3371401cda87a9cf1ec7860ec89
changthatsmynem <nathapab.jirasaweksil@gmail.com>
2023-03-10 14:24:27 +0700

    script js practice

---
+++ script2.js
@@ -0,0 +1,17 @@
+console.log("Hello Javascript");
+
+let server = "server is super gigantic";
+console.log(server);
+
+let firstName = "Chang";
+let lastName = "Spivak";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+let x = 101;
+console.log(typeof x);
+
+x = "Chang";
+console.log(x);
+console.log(typeof x);
