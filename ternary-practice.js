97ed9edb162b53c66046f11253ef239e47544bcd
changthatsmynem <nathapab.jirasaweksil@gmail.com>
2023-03-13 14:50:39 +0700

    Ternary means Third as a base

---
+++ ternary-practice.js
@@ -0,0 +1,7 @@
+const password = "pas$word";
+
+password.length >= 12
+  ? console.log("password is very strong so WELCOME!!")
+  : password.length >= 8
+  ? console.log("WELCOME!")
+  : console.log("SORRY!! the password is too short");
