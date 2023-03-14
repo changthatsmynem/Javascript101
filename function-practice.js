2324bd45947ba2e10fa5cffe5a93b93d945027cb
changthatsmynem <nathapab.jirasaweksil@gmail.com>
2023-03-14 13:35:03 +0700

    add function kept as variable

--- function-practice.js
+++ function-practice.js
@@ -25,3 +25,21 @@ function addNumber(a, b) {
 }
 
 console.log(addNumber(10, 14));
+
+//Function kept as a variable
+function calWithReturn(num1, num2) {
+  return num1 + num2; //allow to install somewhere
+}
+const total = calWithReturn(5, 9);
+console.log(total);
+
+//then total can use later
+function calUseReturn(total) {
+  console.log(total * 5);
+}
+calUseReturn(total);
+
+function cal(a) {
+  console.log(a + 1);
+}
+cal(total);
