dd04bd497793bd96ff23d2c3943727787eb10153
changthatsmynem <nathapab.jirasaweksil@gmail.com>
2023-03-20 22:12:09 +0700

    practice x10

---
+++ DOM/form-event.js
@@ -0,0 +1,110 @@
+let firstName = document.getElementById("fname");
+let lastName = document.getElementById("lname");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let err = document.getElementById("error");
+let submitButton = document.querySelector(".submit-button");
+let form = document.getElementById("form");
+
+const checkValidation = (e) => {
+  if (
+    firstName.value === "" &&
+    lastName.value === "" &&
+    email.value === "" &&
+    password.value === ""
+  ) {
+    e.preventDefault();
+    err.innerHTML = "Please Enter a valid form";
+  }
+};
+
+// form.addEventListener("submit", function (event) {
+//   const emailInput = document.getElementById("email");
+//   const email = emailInput.value;
+
+//   if (!/@/.test(email)) {
+//     alert("Please enter a valid email address.");
+//     event.preventDefault(); // prevent the form from submitting
+//   }
+// });
+
+//K'Luk Code
+form.addEventListener("submit", (e) => {
+  let message = [];
+  if (firstName.value === "" || firstName.value === null) {
+    message.push("Name is required");
+  }
+
+  if (message.length > 0) {
+    e.preventDefault();
+    err.innerHTML = message.join(",");
+  }
+  console.log(firstName);
+});
+
+// const form = document.getElementById("form");
+// const fname = document.getElementById("fname");
+// const lname = document.getElementById("lname");
+// const email = document.getElementById("email");
+// const password = document.getElementById("password");
+// const confirmPassword = document.getElementById("confirmPassword");
+// const error = document.getElementById("error");
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   checkInputs();
+// });
+// function checkInputs() {
+//   const fnameValue = fname.value.trim();
+//   const lnameValue = lname.value.trim();
+//   const emailValue = email.value.trim();
+//   const passwordValue = password.value.trim();
+//   const confirmPasswordValue = confirmPassword.value.trim();
+//   if (fnameValue === "") {
+//     setErrorFor(fname, "First Name cannot be blank");
+//   } else {
+//     setSuccessFor(fname);
+//   }
+//   if (lnameValue === "") {
+//     setErrorFor(lname, "Last Name cannot be blank");
+//   } else {
+//     setSuccessFor(lname);
+//   }
+//   if (emailValue === "") {
+//     setErrorFor(email, "Email cannot be blank");
+//   } else if (!isEmail(emailValue)) {
+//     setErrorFor(email, "Email is not valid");
+//   } else {
+//     setSuccessFor(email);
+//   }
+//   if (passwordValue === "") {
+//     setErrorFor(password, "Password cannot be blank");
+//   } else if (passwordValue.length < 8) {
+//     setErrorFor(password, "Password must be at least 8 characters long");
+//   } else {
+//     setSuccessFor(password);
+//   }
+//   if (confirmPasswordValue === "") {
+//     setErrorFor(confirmPassword, "Confirm Password cannot be blank");
+//   } else if (confirmPasswordValue !== passwordValue) {
+//     setErrorFor(confirmPassword, "Passwords do not match");
+//   } else {
+//     setSuccessFor(confirmPassword);
+//   }
+// }
+// function setErrorFor(input, message) {
+//   const formControl = input.parentElement;
+//   const small = formControl.querySelector("small");
+//   formControl.className = "input error";
+//   small.innerText = message;
+// }
+// function setSuccessFor(input) {
+//   const formControl = input.parentElement;
+//   formControl.className = "input success";
+// }
+// function isEmail(email) {
+//   const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//   return re.test(email);
+// }
+
+// console.log(firstName.value);
--- object-constructor-practice.js
+++ object-constructor-practice.js
@@ -14,3 +14,54 @@ console.log(myCar.drive("Jack"));
 let myCar1 = new Car("Honda", "H435", 2010);
 console.log(myCar1);
 console.log(myCar1.drive("Jill"));
+
+//K'Bank 12
+const ObjChakrit = function (fullname, lastname, role) {
+  this.fullname = fullname;
+  this.lastname = lastname;
+  this.role = role;
+  this.getshowInfo = () => {
+    return `info\nFullname: ${this.fullname}  ${this.lastname}\nrole: ${this.role}`;
+  };
+};
+
+const chakrit = new ObjChakrit("Chakrit", "Kunaruk", "learner");
+
+console.log(chakrit.getshowInfo());
+
+//W3School - tester
+let x = "";
+const myObj = {
+  name: "John",
+  age: 30,
+  cars: [
+    { name: "Ford", models: ["Fiesta", "Focus", "Mustang"] },
+    { name: "BMW", models: ["320", "X3", "X5"] },
+    { name: "Fiat", models: ["500", "Panda"] },
+  ],
+};
+
+for (let i in myObj.cars) {
+  x += "<h2>" + myObj.cars[i].name + "</h2>";
+  for (let j in myObj.cars[i].models) {
+    x += myObj.cars[i].models[j] + "<br>";
+  }
+}
+
+// let x = "";
+// const myObj = {
+//   name: "John",
+//   age: 30,
+//   cars: [
+//     { name: "Ford", models: ["Fiesta", "Focus", "Mustang"] },
+//     { name: "BMW", models: ["320", "X3", "X5"] },
+//     { name: "Fiat", models: ["500", "Panda"] },
+//   ],
+// };
+
+// for (let i in myObj.cars) {
+//   x += `${myObj.cars[i].name}` + "\n";
+//   for (let j in myObj.cars[i].models) {
+//     x += "-" + myObj.cars[i].models[j] + "\n";
+//   }
+// }
